4ed280377d0c5083491d4604d38ffe63c9989db1
Federico Leandro Suarez <federico.uni.89@gmail.com>
2022-12-19 20:19:49 -0300

    Vemos tipos de datos Numbers y sus metodos

---
+++ Tipos de datos/numeros.js
@@ -0,0 +1,15 @@
+let a = 2;
+let b = 1;
+let c = 7.19;
+let d = "5.6";
+console.log(a,b);
+//El metodo toFixed() define la cantidad de decimales de un numero para redondearlo
+console.log(c.toFixed(1));
+
+//El metodo parseInt() me devuelve solo la parte entera 
+console.log(parseInt(c));
+//Tambien me sirve para convertir strings a number
+console.log(parseInt(d));
+
+//El metodo typeof() me sirve para saber el tipo de dato
+console.log(typeof(d));
