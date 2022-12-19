b9717df6c8af9d61d686df8df50388468077f1bd
Federico Leandro Suarez <federico.uni.89@gmail.com>
2022-12-19 08:51:22 -0300

    Vemos Strings y sus metodos

---
+++ Tipos de datos/cadenas_strings.js
@@ -0,0 +1,16 @@
+let myName = "Federico";
+let lastName = "Suarez";
+let fullName = "  Yury  Garcia";
+let text = " lorem ipsum bla ja chon pi um";
+//Concatenación de cadenas de texto
+console.log(myName+" "+lastName); 
+//Averiguar la extensión de una cadena
+console.log("mi nombre tiene "+myName.length+" caracteres");
+//Pasar todo a mayúsculas una cadena 
+console.log("mi nombre todo en mayusculas: "+myName.toUpperCase());
+console.log(fullName);
+//Eliminar los espacios al inicio y al final de una cadena
+console.log("Ahora la misma cadena pero sin espacios:"+fullName.trim());
+//Convertir una cadena a un arreglo
+
+console.log(text.split());
