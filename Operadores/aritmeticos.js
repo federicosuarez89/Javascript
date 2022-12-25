12f374987bf6fabea0558ed61c2ed5f8748aec71
Federico Leandro Suarez <federico.uni.89@gmail.com>
2022-12-24 22:02:14 -0300

    Operadores aritmeticos

---
+++ Operadores/aritmeticos.js
@@ -0,0 +1,22 @@
+/*
+    suma(+)
+    resta(-)
+    multiplicacion(*)
+    division(/)
+    modulo(%) devuelve el resto de una division
+*/
+
+let a = 5;
+let b = 6;
+let c = 20;
+
+console.log(a+b);
+console.log(a-b);
+console.log(a*b);
+console.log(a/b);
+console.log(c%b);
+
+
+
+
+
