e93bfb1a3203965c8b6cba303e404cc29afd0fa4
Federico Leandro Suarez <federico.uni.89@gmail.com>
2022-12-26 01:08:21 -0300

    Ejercicios con condicional if-else

---
+++ Ejercicios/esMayorEdad.js
@@ -0,0 +1,9 @@
+/*
+    Realizamos un programa que determina si una persona es mayor de edad
+*/
+ let Pedro = 36;
+ if (Pedro >=18){
+    console.log(`Pedro tiene ${Pedro} años,es mayor de edad`);
+ }else{
+    console.log(`${Pedro} es menor de edad`);
+ }
---
+++ Ejercicios/numeroParImpar.js
@@ -0,0 +1,9 @@
+/*
+    Determinamos si un numero es par o impar
+*/
+let a = 18;
+if(a %2==0){
+    console.log(`El numero ${a} es par`);
+}else{
+    console.log(`El numero ${a} es impar`);
+}
