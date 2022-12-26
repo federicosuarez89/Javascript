4d4595daf3e9c659bad4c0aca0aaa61641af9a0b
Federico Leandro Suarez <federico.uni.89@gmail.com>
2022-12-26 12:41:58 -0300

    Condicional switch (Udemy)

---
+++ Condicionales/switch.js
@@ -0,0 +1,41 @@
+let mes = 12;
+switch(mes){
+    case 1:
+        console.log("Enero");
+        break;
+    case 2:
+        console.log("Febrero");
+        break;
+    case 3:
+        console.log("Marzo");
+        break;
+    case 4:
+        console.log("Abril");
+        break;
+    case 5:
+        console.log("Mayo");
+        break;
+    case 6:
+        console.log("Junio");
+        break;
+    case 7:
+        console.log("Julio");
+        break;
+    case 8:
+        console.log("Agosto");
+        break;
+    case 9:
+        console.log("Septiembre");
+        break;
+    case 10:
+        console.log("Octubre");
+        break;
+    case 11:
+        console.log("Noviembre");
+        break;
+    case 12:
+        console.log("Diciembre");
+        break;
+    default:
+        console.log("Opcion incorrecta");
+}
