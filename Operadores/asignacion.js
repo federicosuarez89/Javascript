4d612b1005032c752527b1d40c0a8df2dce33434
Federico Leandro Suarez <federico.uni.89@gmail.com>
2022-12-26 00:21:13 -0300

    operadores de asignacion

---
+++ Operadores/asignacion.js
@@ -0,0 +1,27 @@
+//Definimos una variable
+let a = 1;
+let b = 2;
+
+//suma
+a += 3;
+console.log(a);
+
+//resta
+a -= 2;
+console.log(a);
+
+//multiplicacion
+a *= 5;
+console.log(a);
+
+//division
+a /= 2;
+console.log(a);
+
+//modulo
+a %=2;
+console.log(a);
+
+//exponenciacion
+b **= 3
+console.log(b);
