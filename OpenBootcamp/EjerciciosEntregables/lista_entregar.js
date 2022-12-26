c0d7dbcf2a9ce25c554ef0c66a22fe0fe933cb9d
Federico Leandro Suarez <federico.uni.89@gmail.com>
2022-12-26 12:03:11 -0300

    Entregable con tipos de datos (OpenBootcamp)

---
+++ OpenBootcamp/EjerciciosEntregables/lista_entregar.js
@@ -0,0 +1,15 @@
+let nombre = "Federico";
+let edad = 33;
+let desarrollador = true;
+const nacimiento = new Date("july 1 1989");
+const mi_libro = {
+    titulo: "Radical",
+    autor: "David Platt",
+    fecha: 2010,
+    url:"https://www.academia.edu/34832880/David_Platt_Radical_Spanish_Edition_1"
+}
+console.log(nombre);
+console.log(edad);
+console.log(desarrollador);
+console.log(nacimiento);
+console.log(mi_libro);
