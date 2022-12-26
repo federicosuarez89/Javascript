358327a4c9e05fd2f99d57322e477b9bff799ce4
Federico Leandro Suarez <federico.uni.89@gmail.com>
2022-12-26 00:59:21 -0300

    operadores logicos

---
+++ Operadores/logicos.js
@@ -0,0 +1,18 @@
+/*
+    negacion: Not (!)
+              Or (||)
+              And (&&)
+*/
+let a = 10;
+let b = 12;
+let c = true;
+
+//Usamos negacion !
+console.log((a<b));
+console.log(!(a<b));
+
+//Usamos Or ||
+console.log(9===9 || "9"===9);
+
+//Usamos And &&
+console.log(9===9 && "9"===9);
