0013d04d83d5309710a94feed78f673f206393fe
Federico Leandro Suarez <federico.uni.89@gmail.com>
2022-12-26 12:29:15 -0300

    Condicionales if/else if/else (Udemy)

---
+++ Condicionales/if_else_elseif.js
@@ -0,0 +1,6 @@
+let condicion = true;
+if(condicion){
+    console.log("La condicion es verdadera");
+}else{
+    console.log("La condicion no es verdadera");
+}
---
+++ Ejercicios/estacion_anio.js
@@ -0,0 +1,10 @@
+let estacion = 1;
+if(estacion===12 || estacion===1 || estacion===2){
+    console.log("Es verano");
+}else if(estacion===3 || estacion===4 || estacion===5){
+    console.log("Es otoño");
+}else if(estacion===6 || estacion===7 || estacion===8){
+    console.log("Es invierno");
+}else if(estacion===9 || estacion===10 || estacion===11){
+    console.log("Es primavera");
+}
