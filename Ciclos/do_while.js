c08908cbaa51ed9b61ea940b228f0bbc86f2b7f4
Federico Leandro Suarez <federico.uni.89@gmail.com>
2022-12-27 09:32:29 -0300

    Ciclo do-while (Udemy)

---
+++ Ciclos/do_while.js
@@ -0,0 +1,10 @@
+/*
+    Con este ciclo nuestro bloque de codigo al menos se va
+    a ejecutar una vez 
+*/
+
+let contador = 2;
+do{
+    console.log("Valor del contador: "+contador);
+    contador++;
+}while(contador<6);
