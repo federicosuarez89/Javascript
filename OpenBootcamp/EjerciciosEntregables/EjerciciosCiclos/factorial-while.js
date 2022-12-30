8a61389041c70e4b794c3a36f005d8c58bd96530
Federico Leandro Suarez <federico.uni.89@gmail.com>
2022-12-30 13:52:05 -0300

    Entregable factorial de 10 con ciclo while (OpenBootcamp)

--- OpenBootcamp/EjerciciosEntregables/EjerciciosCiclos/factorial-while.js
+++ OpenBootcamp/EjerciciosEntregables/EjerciciosCiclos/factorial-while.js
@@ -1,3 +1,10 @@
 /*
     Este archivo debe calcular el factorial de 10 utilizando un bucle while
 */
+let factorial = 10;
+let resultado = 1;
+while(factorial>0){
+    resultado = factorial * resultado;
+    factorial--;
+    console.log(resultado);
+}
