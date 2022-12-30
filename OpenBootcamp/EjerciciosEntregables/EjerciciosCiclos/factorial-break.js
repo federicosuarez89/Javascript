4ced3f50fe29a9be48ffc489263fb761c3ae9ff1
Federico Leandro Suarez <federico.uni.89@gmail.com>
2022-12-30 13:59:01 -0300

    Entregable factorial de 10 con ciclo while, condicional if y palabra break (OpenBootcamp)

--- OpenBootcamp/EjerciciosEntregables/EjerciciosCiclos/factorial-break.js
+++ OpenBootcamp/EjerciciosEntregables/EjerciciosCiclos/factorial-break.js
@@ -2,3 +2,14 @@
     Este archivo debe calcular el factorial de 10 utilizando un bucle while,
      una bifurcación if y una sentencia break
 */
+let factorial = 10;
+let resultado = 1;
+while(factorial>0){
+    if(factorial<1){
+        break;
+    }else{
+        resultado *= factorial;
+        console.log(resultado);
+        factorial--;
+    }
+}
